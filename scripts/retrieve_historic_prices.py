432691a20f578eaf431612ae2f57eb3a75f60fc4
AJBones <alexbones41@gmail.com>
2026-01-05 20:04:32 +0000

    Python: update script with new dir structure

--- scripts/retrieve_historic_prices.py
+++ scripts/retrieve_historic_prices.py
@@ -5,11 +5,10 @@ import yfinance as yf
 # use a direct grab of the Wikipedia table as a CSV file.
 # Get list of tickers for FTSE 250 index from Wikipedia
 # ftse250_tickers = pd.read_html('https://en.wikipedia.org/wiki/FTSE_250_Index')[2]
+ftse_100_path = "../data/raw/ftse_100.csv"
+ftse_100 = pd.read_csv(ftse_100_path)
 ftse_250_path = "../data/raw/ftse_250.csv"
 ftse_250 = pd.read_csv(ftse_250_path)
-
-ftse_100_path = "../data/raw/ftse_100.csv"
-ftse_100 = pd.read_csv(ftse_250_path)
 
 # Time to retrieve the data for each of the constituents for modelling
 price_data_100 = pd.DataFrame()
@@ -24,10 +23,10 @@ for i in range(len(ftse_100)):
     data.columns = data.columns.droplevel(1)
     data["Company"] = ftse_100.loc[i, "Company"]
     data["Ticker"] = ftse_100.loc[i, "Ticker"]
-    data["Sector"] = ftse_100.loc[i, "FTSE Industry Classification Benchmark sector"]
+    data["Sector"] = ftse_100.loc[i, "FTSE industry classification benchmark sector"]
     data.reset_index(inplace=True)
     price_data_100 = pd.concat([price_data_100, data], axis=0, ignore_index=True)
-price_data_100.to_csv("../data/ftse_100_historic_prices.csv")
+price_data_100.to_csv("../data/raw/ftse_100_historic_prices.csv", index=False)
 
 print("Getting data for FTSE 250 companies...")
 for i in range(len(ftse_250)):
@@ -39,5 +38,5 @@ for i in range(len(ftse_250)):
     data["Ticker"] = ftse_250.loc[i, "Ticker"]
     data["Sector"] = ftse_250.loc[i, "FTSE Industry Classification Benchmark sector"]
     data.reset_index(inplace=True)
-    price_data = pd.concat([price_data_250, data], axis=0, ignore_index=True)
-price_data_250.to_csv("../data/ftse_250_historic_prices.csv")
+    price_data_250 = pd.concat([price_data_250, data], axis=0, ignore_index=True)
+price_data_250.to_csv("../data/raw/ftse_250_historic_prices.csv", index=False)
