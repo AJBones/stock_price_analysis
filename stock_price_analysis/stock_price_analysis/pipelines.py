f9449af975491b1eac0207830686b28acec41d79
AJBones <alexbones41@gmail.com>
2026-09-21 20:58:46 +0100

    python: add day of week encoding

--- stock_price_analysis/stock_price_analysis/pipelines.py
+++ stock_price_analysis/stock_price_analysis/pipelines.py
@@ -28,7 +28,9 @@ class DynamicCyclicalEncoder(BaseEstimator, TransformerMixin):
         X_out["month_sin"] = np.sin(2 * np.pi * dates.dt.month / 12)
         X_out["month_cos"] = np.cos(2 * np.pi * dates.dt.month / 12)
 
-        # TODO: Add encoding for day of week
+        # 4. Add encoding for day of week
+        X_out["dow_sin"] = np.sin(2 * np.pi * dates.dt.dayofweek / 7)
+        X_out["dow_cos"] = np.cos(2 * np.pi * dates.dt.dayofweek / 7)
 
         # Drop the original date column to keep it clean for sklearn
         return X_out.drop(columns=[self.date_column])
